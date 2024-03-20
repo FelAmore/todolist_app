575c473094dee759b4e47a182e1978fc57017c13
FelAmore <114073117+FelAmore@users.noreply.github.com>
2024-03-20 20:31:38 +0700

    Add files via upload

---
+++ src/components/Task/index.jsx
@@ -0,0 +1,26 @@
+import styles from './task.module.css';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { CiEdit } from 'react-icons/ci';
+import { TbTrash } from 'react-icons/tb';
+
+export function Task({ task, onDelete, onComplete }) {
+  return (
+    <div className={styles.task}>
+      <button className={styles.checkContainer} onClick={() => onComplete(task.id)}>
+        {task.isCompleted ? <BsFillCheckCircleFill /> : <div />}
+      </button>
+
+      <p className={task.isCompleted ? styles.textCompleted : ""}>
+        {task.title}
+      </p>
+
+{/*       <button className={styles.editButton} onClick={() => onEdit(task.id)}> */}
+{/*         <CiEdit size={20} /> */}
+{/*       </button> */}
+
+      <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
+        <TbTrash size={20} />
+      </button>
+    </div>
+  )
+}
