5fc0cc30248de30669dea361b131708025eb5898
FelAmore <114073117+FelAmore@users.noreply.github.com>
2024-05-13 18:30:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,jsx}'],
+module.exports = {
+  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
   theme: {
     extend: {},
   },
